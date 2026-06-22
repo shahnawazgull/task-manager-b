9ce5cb463d018cada993b7687f8f5f4a53ce64c6
Shahnawaz <shahnawazgull2166@gmail.com>
2026-06-22 14:45:30 +0500

    deployment fixes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import taskRoutes from './routes/task.routes.js'
 
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use(cors())
